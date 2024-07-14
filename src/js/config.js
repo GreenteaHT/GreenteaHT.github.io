c847b07bec897257e7c218486d82a55a5604f9f9
Greentea_latte <44830849+GreenteaHT@users.noreply.github.com>
2024-07-15 01:24:35 +0900

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,11 @@
+// [주의] 붙여넣기 하는 과정에서 export 키워드를 삭제하지 않도록 주의하세요!
+export const firebaseConfig = {
+  apiKey: "AIzaSyAMve8oUi4q04i6v031zaWfms6O9zLPiGQ",
+  authDomain: "vaco-messenger-d7f75.firebaseapp.com",
+  databaseURL: "https://vaco-messenger-d7f75-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  projectId: "vaco-messenger-d7f75",
+  storageBucket: "vaco-messenger-d7f75.appspot.com",
+  messagingSenderId: "182407811504",
+  appId: "1:182407811504:web:5fe5422ab29fa8adb3c675",
+  measurementId: ""
+};
